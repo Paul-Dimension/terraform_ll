9a3b2b74d6140753f1cba6422498da3c01a77510
PAVLO DEMENSHYN <89635409+Paul-Dimension@users.noreply.github.com>
2025-06-08 17:29:57 -0300

    autoscaling

--- main.tf
+++ main.tf
@@ -51,21 +51,25 @@ module "blog_sg" {
   egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
-# EC2 instance (Tomcat)
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-  subnet_id              = module.blog_vpc.public_subnets[0]
-  associate_public_ip_address = true
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "8.3.0"
 
-  tags = {
-    Name = "HelloWorld"
-  }
+  # insert the 1 required variable here
+  name     = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns = module.blog_alb.target_group_arns
+  security_groups = [module.blog_sg.security_group_id]
+
+  image_id               = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
 }
 
 # ALB (Application Load Balancer)
-module "alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0" # Используем более стабильную версию
 
