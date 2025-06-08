81b7bc3fa811add2dd42d1e45b7384420cb25beb
PAVLO DEMENSHYN <89635409+Paul-Dimension@users.noreply.github.com>
2025-06-08 16:37:11 -0300

    Create new dev vpc

--- main.tf
+++ main.tf
@@ -15,52 +15,52 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-# Получение VPC по умолчанию
-data "aws_vpc" "default" {
-  default = true
-}
-
+# VPC модуль
 module "blog_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.2" # добавь, если хочешь явно
 
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "eus-west-2b", "us-west-2c"]
-
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs            = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
 
-# Подключение готового модуля security group с доступом по 80/443 и всем выходящим трафиком
+# Security group модуль
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
-  name    = "blog_sg"
-  vpc_id  = module.blog_vpc_id
 
-  # Разрешаем входящий HTTP и HTTPS трафик от всех
+  name   = "blog_sg"
+  vpc_id = module.blog_vpc.vpc_id
+
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  # Разрешаем весь исходящий трафик
   egress_rules        = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
-# EC2 инстанс с использованием модуля SG
+# EC2 instance
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  subnet_id = module.blog_vpc.public_subnets[0]
+  subnet_id              = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+# Переменная типа инстанса
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default     = "t3.nano"
 }
