e72bcb6d9b5a8f554fb452469254cf9237c9986b
PAVLO DEMENSHYN <89635409+Paul-Dimension@users.noreply.github.com>
2025-06-08 16:14:22 -0300

    method of module using

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Получение актуального AMI от Bitnami с Tomcat
 data "aws_ami" "app_ami" {
   most_recent = true
 
@@ -14,14 +15,31 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+# Получение VPC по умолчанию
 data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+# Подключение готового модуля security group с доступом по 80/443 и всем выходящим трафиком
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.0"
+  name    = "blog_sg"
+  vpc_id  = data.aws_vpc.default.id
 
+  # Разрешаем входящий HTTP и HTTPS трафик от всех
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  # Разрешаем весь исходящий трафик
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
+}
+
+# EC2 инстанс с использованием модуля SG
+resource "aws_instance" "blog" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -29,53 +47,8 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.3.0"
-  name = "blog_new"
-
-  vpc_id = data.aws_vpc.default.id
-
-  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["o.o.o.o/o"]
-
-  egress_rules       = ["all-all"]
-  egress_cidr_blocks = ["o.o.o.o/o"]
-}
-
-resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = "Allow http and https in. Allow everything out"
-
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "blog_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
+# Тип инстанса как переменная
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default     = "t3.nano"
 }
